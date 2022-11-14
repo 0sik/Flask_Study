7817c93d279cb30acd0ccec3e053e523c3a646b0
0sik <ghdtlrdudtlr@naver.com>
2022-11-14 23:58:20 +0900

    [ADD](집등록하기)플라스크 공부 html 등록

---
+++ application.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import sys
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template("hello.html")
+    
+@application.route("/apply")
+def apply():
+    return render_template("apply.html")
+    
+@application.route("/list")
+def list():
+    return render_template("list.html")
+
+if __name__ == "__main__":
+    application.run(port=5001,debug=True)
